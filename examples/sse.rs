a9c2e7ccc179cf1f318ad3bad6fc38fff2c0196e
Matthias Seitz <matthias.seitz@outlook.de>
2023-04-24 22:23:33 +0200

    chore: add tracing subscriber

--- examples/sse.rs
+++ examples/sse.rs
@@ -1,9 +1,12 @@
 //! Basic SSE example
 use futures_util::StreamExt;
 use mev_share_rs::EventClient;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry().with(fmt::layer()).with(EnvFilter::from_default_env()).init();
+
     let mainnet_sse = "https://mev-share.flashbots.net";
     let client = EventClient::default();
     let mut stream = client.subscribe(mainnet_sse).await.unwrap();
