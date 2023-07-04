c92f123cdc8ee471e99abbd99e2b033aba35d47b
frankie <88221686+FrankieIsLost@users.noreply.github.com>
2023-07-03 18:29:18 -0700

    custom type

--- crates/mev-share-rpc-api/src/types.rs
+++ crates/mev-share-rpc-api/src/types.rs
@@ -1,7 +1,9 @@
 //! MEV-share bundle type bindings
 
 use ethers_core::types::{Bytes, U64, TxHash, Address};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Deserializer};
+use serde::ser::{Serializer, SerializeSeq};
+
 
 /// A bundle of transactions to send to the matchmaker.
 #[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
@@ -97,29 +99,156 @@ pub struct RefundConfig {
 pub struct Privacy {
     /// Hints on what data should be shared about the bundle and its transactions
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub hints: Option<Vec<PrivacyHint>>,
+    pub hints: Option<PrivacyHint>,
     /// The addresses of the builders that should be allowed to see the bundle.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub builders: Option<Vec<Address>>,
 }
 
 /// Hints on what data should be shared about the bundle and its transactions
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub enum PrivacyHint {
-    /// The calldata of the transaction.
-    Calldata,
-    /// The address of the contract the transaction is calling.
-    ContractAddress,
-    /// The logs emitted by the transaction.
-    Logs,
-    /// The 4-byte function selector of the transaction.
-    FunctionSelector,
-    /// The hash of the transaction.
-    Hash,
+#[derive(Clone, Debug, PartialEq)]
+#[derive(Default)]
+pub struct PrivacyHint {
+    /// The calldata of the bundle's transactions should be shared.
+    pub calldata: bool,
+    /// The address of the bundle's transactions should be shared.
+    pub contract_address: bool,
+    /// The logs of the bundle's transactions should be shared.
+    pub logs: bool,
+    /// The function selector of the bundle's transactions should be shared.
+    pub function_selector: bool,
+    /// The hash of the bundle's transactions should be shared.
+    pub hash: bool,
+    /// The hash of the bundle should be shared.
+    pub tx_hash: bool,
 }
 
 
+
+#[allow(missing_docs)]
+impl PrivacyHint {
+
+    pub fn with_calldata(mut self) -> Self {
+        self.calldata = true;
+        self
+    }
+
+    pub fn with_contract_address(mut self) -> Self {
+        self.contract_address = true;
+        self
+    }
+
+    pub fn with_logs(mut self) -> Self {
+        self.logs = true;
+        self
+    }
+
+    pub fn with_function_selector(mut self) -> Self {
+        self.function_selector = true;
+        self
+    }
+
+    pub fn with_hash(mut self) -> Self {
+        self.hash = true;
+        self
+    }
+
+    pub fn with_tx_hash(mut self) -> Self {
+        self.tx_hash = true;
+        self
+    }
+
+    pub fn has_calldata(&self) -> bool {
+        self.calldata
+    }
+
+    pub fn has_contract_address(&self) -> bool {
+        self.contract_address
+    }
+
+    pub fn has_logs(&self) -> bool {
+        self.logs
+    }
+
+    pub fn has_function_selector(&self) -> bool {
+        self.function_selector
+    }
+
+    pub fn has_hash(&self) -> bool {
+        self.hash
+    }
+
+    pub fn has_tx_hash(&self) -> bool {
+        self.tx_hash
+    }
+
+    fn num_hints(&self) -> usize {
+        let mut num_hints = 0;
+        if self.calldata {
+            num_hints += 1;
+        }
+        if self.contract_address {
+            num_hints += 1;
+        }
+        if self.logs {
+            num_hints += 1;
+        }
+        if self.function_selector {
+            num_hints += 1;
+        }
+        if self.hash {
+            num_hints += 1;
+        }
+        if self.tx_hash {
+            num_hints += 1;
+        }
+        num_hints
+    }
+}
+
+impl Serialize for PrivacyHint {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut seq = serializer.serialize_seq(Some(self.num_hints()))?;
+        if self.calldata {
+            seq.serialize_element("calldata")?;
+        }
+        if self.contract_address {
+            seq.serialize_element("contract_address")?;
+        }
+        if self.logs {
+            seq.serialize_element("logs")?;
+        }
+        if self.function_selector {
+            seq.serialize_element("function_selector")?;
+        }
+        if self.hash {
+            seq.serialize_element("hash")?;
+        }
+        if self.tx_hash {
+            seq.serialize_element("tx_hash")?;
+        }
+        seq.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for PrivacyHint {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let hints = Vec::<String>::deserialize(deserializer)?;
+        let mut privacy_hint = PrivacyHint::default();
+        for hint in hints {
+            match hint.as_str() {
+                "calldata" => privacy_hint.calldata = true,
+                "contract_address" => privacy_hint.contract_address = true,
+                "logs" => privacy_hint.logs = true,
+                "function_selector" => privacy_hint.function_selector = true,
+                "hash" => privacy_hint.hash = true,
+                "tx_hash" => privacy_hint.tx_hash = true,
+                _ => return Err(serde::de::Error::custom("invalid privacy hint")),
+            }
+        }
+        Ok(privacy_hint)
+    }
+}
 
 /// Response from the matchmaker after sending a bundle.
 #[derive(Deserialize, Debug, Serialize, Clone, PartialEq)]
@@ -258,10 +387,15 @@ mod tests {
             }]),
             ..Default::default()
         });
+
         let privacy = Some(Privacy {
-            hints: Some(vec![PrivacyHint::Calldata]),
+            hints: Some(PrivacyHint {
+                calldata: true,
+                ..Default::default()
+            }),
             ..Default::default()
         });
+       
         let bundle = SendBundleRequest {
             protocol_version: ProtocolVersion::V0_1,
             inclusion: Inclusion {
