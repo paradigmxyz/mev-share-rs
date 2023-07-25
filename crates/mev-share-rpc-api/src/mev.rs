b5838b2bd692b17cbab78bb124637fed48b79b47
Matthias Seitz <matthias.seitz@outlook.de>
2023-07-25 22:29:53 +0200

    mev-api ext

--- crates/mev-share-rpc-api/src/mev.rs
+++ crates/mev-share-rpc-api/src/mev.rs
@@ -1,6 +1,5 @@
-use jsonrpsee::{core::RpcResult, proc_macros::rpc};
-
 use crate::{SendBundleRequest, SendBundleResponse, SimBundleOverrides, SimBundleResponse};
+use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 
 /// Mev rpc interface.
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "mev"))]
@@ -21,4 +20,58 @@ pub trait MevApi {
         bundle: SendBundleRequest,
         sim_overrides: SimBundleOverrides,
     ) -> RpcResult<SimBundleResponse>;
+}
+
+#[async_trait::async_trait]
+pub trait MevApiExt {
+    async fn send_bundle_request(
+        &self,
+        request: SendBundleRequest,
+    ) -> Result<SendBundleResponse, jsonrpsee::core::Error>;
+}
+
+#[async_trait::async_trait]
+impl<T> MevApiExt for T
+where
+    T: MevApiClient + Sync + Send,
+{
+    async fn send_bundle_request(
+        &self,
+        request: SendBundleRequest,
+    ) -> Result<SendBundleResponse, jsonrpsee::core::Error> {
+        self.send_bundle(request).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::FlashbotsSignerLayer;
+    use ethers_core::rand::thread_rng;
+    use ethers_signers::LocalWallet;
+    use jsonrpsee::http_client::{transport, HttpClient, HttpClientBuilder};
+
+    #[tokio::test]
+    async fn test_box() {
+        let fb_signer = LocalWallet::new(&mut thread_rng());
+
+        let http = HttpClientBuilder::default()
+            .set_middleware(
+                tower::ServiceBuilder::new()
+                    .map_err(transport::Error::Http)
+                    .layer(FlashbotsSignerLayer::new(fb_signer.clone())),
+            )
+            .build("http://localhost:3030")
+            .unwrap();
+        let b = Box::new(http) as Box<dyn MevApiExt>;
+        b.send_bundle_request(SendBundleRequest {
+            protocol_version: Default::default(),
+            inclusion: Default::default(),
+            bundle_body: vec![],
+            validity: None,
+            privacy: None,
+        })
+        .await
+        .unwrap();
+    }
 }
