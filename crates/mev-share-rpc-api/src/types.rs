dea8b5419522e2bea6fa9f461f5da49c13a300eb
optimiz-r <optimizooor@protonmail.com>
2023-08-15 01:31:34 +0200

    fix: make `flashbots_*` fields pub

--- crates/mev-share-rpc-api/src/types.rs
+++ crates/mev-share-rpc-api/src/types.rs
@@ -476,12 +476,12 @@ impl<'de> Deserialize<'de> for BundleStats {
 pub struct StatsSeen {
     /// boolean representing if this searcher has a high enough reputation to be in the high
     /// priority queue
-    is_high_priority: bool,
+    pub is_high_priority: bool,
     /// representing whether the bundle gets simulated. All other fields will be omitted except
     /// simulated field if API didn't receive bundle
-    is_simulated: bool,
+    pub is_simulated: bool,
     /// time at which the bundle API received the bundle
-    received_at: String,
+    pub received_at: String,
 }
 
 /// Response for `flashbots_getBundleStatsV2` represents stats for a single bundle
@@ -494,21 +494,21 @@ pub struct StatsSeen {
 pub struct StatsSimulated {
     /// boolean representing if this searcher has a high enough reputation to be in the high
     /// priority queue
-    is_high_priority: bool,
+    pub is_high_priority: bool,
     /// representing whether the bundle gets simulated. All other fields will be omitted except
     /// simulated field if API didn't receive bundle
-    is_simulated: bool,
+    pub is_simulated: bool,
     /// time at which the bundle gets simulated
-    simulated_at: String,
+    pub simulated_at: String,
     /// time at which the bundle API received the bundle
-    received_at: String,
+    pub received_at: String,
     /// indicates time at which each builder selected the bundle to be included in the target
     /// block
     #[serde(default = "Vec::new")]
-    considered_by_builders_at: Vec<ConsideredByBuildersAt>,
+    pub considered_by_builders_at: Vec<ConsideredByBuildersAt>,
     /// indicates time at which each builder sealed a block containing the bundle
     #[serde(default = "Vec::new")]
-    sealed_by_builders_at: Vec<SealedByBuildersAt>,
+    pub sealed_by_builders_at: Vec<SealedByBuildersAt>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -533,28 +533,28 @@ pub struct SealedByBuildersAt {
 pub struct UserStats {
     /// Represents whether this searcher has a high enough reputation to be in the high priority
     /// queue.
-    is_high_priority: bool,
+    pub is_high_priority: bool,
     /// The total amount paid to validators over all time.
     #[serde(with = "u256_numeric_string")]
-    all_time_validator_payments: U256,
+    pub all_time_validator_payments: U256,
     /// The total amount of gas simulated across all bundles submitted to Flashbots.
     /// This is the actual gas used in simulations, not gas limit.
     #[serde(with = "u256_numeric_string")]
-    all_time_gas_simulated: U256,
+    pub all_time_gas_simulated: U256,
     /// The total amount paid to validators the last 7 days.
     #[serde(with = "u256_numeric_string")]
-    last_7d_validator_payments: U256,
+    pub last_7d_validator_payments: U256,
     /// The total amount of gas simulated across all bundles submitted to Flashbots in the last 7
     /// days. This is the actual gas used in simulations, not gas limit.
     #[serde(with = "u256_numeric_string")]
-    last_7d_gas_simulated: U256,
+    pub last_7d_gas_simulated: U256,
     /// The total amount paid to validators the last day.
     #[serde(with = "u256_numeric_string")]
-    last_1d_validator_payments: U256,
+    pub last_1d_validator_payments: U256,
     /// The total amount of gas simulated across all bundles submitted to Flashbots in the last
     /// day. This is the actual gas used in simulations, not gas limit.
     #[serde(with = "u256_numeric_string")]
-    last_1d_gas_simulated: U256,
+    pub last_1d_gas_simulated: U256,
 }
 
 /// Bundle of transactions for `eth_sendBundle`
