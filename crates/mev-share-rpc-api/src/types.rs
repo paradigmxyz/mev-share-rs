542e6ef8a89dd021aa7d3c424dadaf811520076e
optimiz-r <optimizooor@protonmail.com>
2023-08-14 22:40:13 +0200

    feature: add `flashbots_*` rpc
    adds onchain example: stats cannot be inspected for a bundle that has not been seen onchain

--- crates/mev-share-rpc-api/src/types.rs
+++ crates/mev-share-rpc-api/src/types.rs
@@ -418,28 +418,97 @@ pub struct CancelPrivateTransactionRequest {
     pub tx_hash: H256,
 }
 
+// TODO(@optimiz-r): Revisit after <https://github.com/flashbots/flashbots-docs/issues/424> is closed.
 /// Response for `flashbots_getBundleStatsV2` represents stats for a single bundle
 ///
 /// Note: this is V2: <https://docs.flashbots.net/flashbots-auction/searchers/advanced/rpc-endpoint#flashbots_getbundlestatsv2>
 ///
 /// Timestamp format: "2022-10-06T21:36:06.322Z"
+#[derive(Default, Debug, Clone, PartialEq)]
+pub enum BundleStats {
+    /// The relayer has not yet seen the bundle.
+    #[default]
+    Unknown,
+    /// The relayer has seen the bundle, but has not simulated it yet.
+    Seen(StatsSeen),
+    /// The relayer has seen the bundle and has simulated it.
+    Simulated(StatsSimulated),
+}
+
+impl Serialize for BundleStats {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self {
+            BundleStats::Unknown => serde_json::json!({"isSimulated": false}).serialize(serializer),
+            BundleStats::Seen(stats) => stats.serialize(serializer),
+            BundleStats::Simulated(stats) => stats.serialize(serializer),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for BundleStats {
+    fn deserialize<D>(deserializer: D) -> Result<BundleStats, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let map = serde_json::Map::deserialize(deserializer)?;
+
+        if map.get("receivedAt").is_none() {
+            Ok(BundleStats::Unknown)
+        } else if map["isSimulated"] == "false" {
+            StatsSeen::deserialize(serde_json::Value::Object(map))
+                .map(BundleStats::Seen)
+                .map_err(serde::de::Error::custom)
+        } else {
+            StatsSimulated::deserialize(serde_json::Value::Object(map))
+                .map(BundleStats::Simulated)
+                .map_err(serde::de::Error::custom)
+        }
+    }
+}
+
+/// Response for `flashbots_getBundleStatsV2` represents stats for a single bundle
+///
+/// Note: this is V2: <https://docs.flashbots.net/flashbots-auction/searchers/advanced/rpc-endpoint#flashbots_getbundlestatsv2>
+///
+/// Timestamp format: "2022-10-06T21:36:06.322Z
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct UserStatsV2 {
+pub struct StatsSeen {
     /// boolean representing if this searcher has a high enough reputation to be in the high
     /// priority queue
-    pub is_high_priority: bool,
+    is_high_priority: bool,
     /// representing whether the bundle gets simulated. All other fields will be omitted except
     /// simulated field if API didn't receive bundle
-    pub is_simulated: bool,
-    /// time at which the bundle gets simulated
-    pub simulated_at: String,
+    is_simulated: bool,
     /// time at which the bundle API received the bundle
-    pub received_at: String,
-    /// indicates time at which each builder selected the bundle to be included in the target block
-    pub considered_by_builders_at: Vec<ConsideredByBuildersAt>,
+    received_at: String,
+}
+
+/// Response for `flashbots_getBundleStatsV2` represents stats for a single bundle
+///
+/// Note: this is V2: <https://docs.flashbots.net/flashbots-auction/searchers/advanced/rpc-endpoint#flashbots_getbundlestatsv2>
+///
+/// Timestamp format: "2022-10-06T21:36:06.322Z
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StatsSimulated {
+    /// boolean representing if this searcher has a high enough reputation to be in the high
+    /// priority queue
+    is_high_priority: bool,
+    /// representing whether the bundle gets simulated. All other fields will be omitted except
+    /// simulated field if API didn't receive bundle
+    is_simulated: bool,
+    /// time at which the bundle gets simulated
+    simulated_at: String,
+    /// time at which the bundle API received the bundle
+    received_at: String,
+    /// indicates time at which each builder selected the bundle to be included in the target
+    /// block
+    #[serde(default = "Vec::new")]
+    considered_by_builders_at: Vec<ConsideredByBuildersAt>,
     /// indicates time at which each builder sealed a block containing the bundle
-    pub sealed_by_builders_at: Vec<SealedByBuildersAt>,
+    #[serde(default = "Vec::new")]
+    sealed_by_builders_at: Vec<SealedByBuildersAt>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -454,6 +523,38 @@ pub struct ConsideredByBuildersAt {
 pub struct SealedByBuildersAt {
     pub pubkey: String,
     pub timestamp: String,
+}
+
+/// Response for `flashbots_getUserStatsV2` represents stats for a searcher.
+///
+/// Note: this is V2: <https://docs.flashbots.net/flashbots-auction/searchers/advanced/rpc-endpoint#flashbots_getuserstatsv2>
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UserStats {
+    /// Represents whether this searcher has a high enough reputation to be in the high priority
+    /// queue.
+    is_high_priority: bool,
+    /// The total amount paid to validators over all time.
+    #[serde(with = "u256_numeric_string")]
+    all_time_validator_payments: U256,
+    /// The total amount of gas simulated across all bundles submitted to Flashbots.
+    /// This is the actual gas used in simulations, not gas limit.
+    #[serde(with = "u256_numeric_string")]
+    all_time_gas_simulated: U256,
+    /// The total amount paid to validators the last 7 days.
+    #[serde(with = "u256_numeric_string")]
+    last_7d_validator_payments: U256,
+    /// The total amount of gas simulated across all bundles submitted to Flashbots in the last 7
+    /// days. This is the actual gas used in simulations, not gas limit.
+    #[serde(with = "u256_numeric_string")]
+    last_7d_gas_simulated: U256,
+    /// The total amount paid to validators the last day.
+    #[serde(with = "u256_numeric_string")]
+    last_1d_validator_payments: U256,
+    /// The total amount of gas simulated across all bundles submitted to Flashbots in the last
+    /// day. This is the actual gas used in simulations, not gas limit.
+    #[serde(with = "u256_numeric_string")]
+    last_1d_gas_simulated: U256,
 }
 
 /// Bundle of transactions for `eth_sendBundle`
