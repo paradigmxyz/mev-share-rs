56f6bb2dbe8fc24e6555adfc9e584ebbbb752510
Enrique Ortiz <hi@enriqueortiz.dev>
2023-09-20 00:52:33 -0400

    chore: make clippy happy on vec usage

--- examples/rpc-client.rs
+++ examples/rpc-client.rs
@@ -46,9 +46,8 @@ async fn main() {
     let bytes = tx.rlp_signed(&signature);
 
     // Build bundle
-    let mut bundle_body = Vec::new();
-    bundle_body.push(BundleItem::Hash { hash: tx_hash });
-    bundle_body.push(BundleItem::Tx { tx: bytes, can_revert: false });
+    let bundle_body =
+        vec![BundleItem::Hash { hash: tx_hash }, BundleItem::Tx { tx: bytes, can_revert: false }];
 
     let bundle = SendBundleRequest { bundle_body, ..Default::default() };
 
